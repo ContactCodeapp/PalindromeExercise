c5dff1cc0abc90f3fcea9ea55561265263a41562
contactcodeapp <ddougherty@contactcodeapp.com>
2021-04-26 21:51:23 -0400

    completed unit testing for palindrones

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+        }
+
+        public bool IsAPalindrome(string input)
+        {
+            char[] arry = input.ToCharArray();
+            Array.Reverse(arry);
+            var flip = new string(arry);
+
+            return (input == flip);
+
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("anna", true)]
+        [InlineData("jeff", false)]
+        [InlineData("radar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("penny", false)]
+        public void Test1(string input, bool expected)
         {
+            //arrange
+            var t = new WordSmith();
 
+            //act
+            var actual = t.IsAPalindrome(input);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
